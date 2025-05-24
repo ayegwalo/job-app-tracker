eaa9307f2f82f7d9c7b2329ad5979c9f3cc3f2ef
chris.relaxier@gmail.com <chris.relaxier@gmail.com>
2025-05-25 00:55:59 +0400

    fix(env): load Appwrite config from .env using VITE_ vars

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+
+
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+export const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
